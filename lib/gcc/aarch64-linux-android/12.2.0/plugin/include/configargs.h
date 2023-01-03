9991c2b02fb8f82cce62f528261e3f736f00e814
nn-firc <2319221757@qq.com>
2023-01-04 01:33:17 +0800

    - init

---
+++ lib/gcc/aarch64-linux-android/12.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/work/ndk-toolchain-gcc-12/ndk-toolchain-gcc-12/tmp/gcc-12.2.0/configure --host=x86_64-linux-gnu --build=x86_64-linux-gnu --target=aarch64-linux-android --disable-shared --disable-nls --enable-default-pie --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --with-gnu-as --with-gnu-ld --disable-libstdc__-v3 --disable-tls --disable-ssp --disable-bootstrap --enable-initfini-array --enable-libatomic-ifuncs=no --prefix=/home/runner/work/ndk-toolchain-gcc-12/ndk-toolchain-gcc-12/tmp/newer-toolchain --with-gmp --with-mpfr --with-mpc --with-system-zlib --enable-languages=c,c++,fortran --enable-plugins --enable-libgomp --enable-gnu-indirect-function --disable-libcilkrts --disable-libsanitizer --enable-gold --enable-threads --enable-eh-frame-hdr-for-static --enable-graphite=yes --with-isl --disable-multilib --enable-fix-cortex-a53-835769 --enable-fix-cortex-a53-843419 --with-sysroot=/home/runner/work/ndk-toolchain-gcc-12/ndk-toolchain-gcc-12/tmp/newer-toolchain/sysroot --with-gxx-include-dir=/home/runner/work/ndk-toolchain-gcc-12/ndk-toolchain-gcc-12/tmp/newer-toolchain/include/c++/12.2.0";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
